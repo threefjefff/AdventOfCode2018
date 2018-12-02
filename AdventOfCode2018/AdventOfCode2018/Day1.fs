5002660af5d17a2c74c5dfc287064faf2c1b0f5e
Unknown <threefjefff@gmail.com>
2018-12-02 13:39:34 +0000

    Part 2 (not repeating list)

--- AdventOfCode2018/AdventOfCode2018/Day1.fs
+++ AdventOfCode2018/AdventOfCode2018/Day1.fs
@@ -11,6 +11,27 @@ module Day1 =
             readFile filepath
         | _ ->
             List.map int args 
-    
+
+    let runningTotal (inputs: int list) =
+        inputs 
+        |> List.mapFold (fun total next -> (total + next, total + next)) 0
+
+    let rec findDuplicate list =
+        match list with
+        | first::tail ->
+            match List.exists (fun x -> x == first) tail with
+            | true ->
+                Some(first)
+            | false ->
+                findDuplicate tail
+        | [] ->
+            None
+
     let execute (args: string list)=
-        inputAsInts args |> List.sum |> printfn "Total is %d"
+        let result = inputAsInts args |> runningTotal
+        result |> snd |> printfn "Total is %d"
+        result 
+        |> fst 
+        |> findDuplicate
+        |> printfn "Running list is %A"
+        
