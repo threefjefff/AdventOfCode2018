11706cc2bca21de05c8b30eaa690924e14ce2514
Unknown <threefjefff@gmail.com>
2018-12-02 11:53:34 +0000

    Initial structure + Day 1/1

---
+++ AdventOfCode2018/AdventOfCode2018/Day1.fs
@@ -0,0 +1,16 @@
+﻿namespace AdventOfCode
+open System.IO
+module Day1 = 
+    let readFile filepath =
+        File.ReadAllLines filepath |> List.ofSeq |> List.map int
+
+    let inputAsInts args =
+        match args with
+        | [filepath] ->
+            printfn "Attempting to read filepath %s" filepath
+            readFile filepath
+        | _ ->
+            List.map int args 
+    
+    let execute (args: string list)=
+        inputAsInts args |> List.sum |> printfn "Total is %d"
---
+++ AdventOfCode2018/AdventOfCode2018/Program.fs
@@ -0,0 +1,26 @@
+﻿open AdventOfCode
+
+let dayArg argList = 
+    match argList with
+    | [] ->
+        None
+    | first :: rest ->
+        Some(first |> int, rest)
+
+let executeDay day args =
+    match day with
+    | 1 ->
+        printfn "day 1, args: %A" args
+        Day1.execute args
+    | _ ->
+        printfn "This day isn't ready yet!"
+
+[<EntryPoint>]
+let main argv =
+    let day = List.ofSeq argv |> dayArg
+    match day with
+    | Some (day, additionalArgs) ->
+        executeDay day additionalArgs
+    | None ->
+        printfn "Enter with <day> argument to run that day's solution!"
+    0 // Return an integer exit code
